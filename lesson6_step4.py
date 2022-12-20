97d727e080d4376809b1d35c127317a7e7ce3ac7
Sergey Voron <sergeyv39@gmail.com>
2022-12-20 08:56:53 +0200

    change line separator in pycharm

--- lesson6_step4.py
+++ lesson6_step4.py
@@ -1,1 +1,27 @@
-from selenium import webdriverimport mathimport timefrom selenium.webdriver.common.by import Byfrom selenium.webdriver.support.ui import WebDriverWaitfrom selenium.webdriver.support import expected_conditions as ECtry:    link = "http://suninjuly.github.io/explicit_wait2.html"    browser = webdriver.Chrome()    browser.get(link)    price = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, 'price'), '$100'))    button = browser.find_element(By.ID, 'book')    button.click()    x = browser.find_element(By.ID, 'input_value')    total = str(math.log(abs(12 * math.sin(int(x.text)))))    answer = browser.find_element(By.ID, 'answer')    answer.send_keys(total)    submit = browser.find_element(By.ID, 'solve')    submit.click()finally:    # ожидание чтобы визуально оценить результаты прохождения скрипта    time.sleep(10)    # закрываем браузер после всех манипуляций    browser.quit()# не забываем оставить пустую строку в конце файла
+from selenium import webdriver
+import math
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    price = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, 'price'), '$100'))
+    button = browser.find_element(By.ID, 'book')
+    button.click()
+    x = browser.find_element(By.ID, 'input_value')
+    total = str(math.log(abs(12 * math.sin(int(x.text)))))
+    answer = browser.find_element(By.ID, 'answer')
+    answer.send_keys(total)
+    submit = browser.find_element(By.ID, 'solve')
+    submit.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+    # не забываем оставить пустую строку в конце файла
